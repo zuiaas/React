f92e3ca1817bb50bfdae37ea5584e3d242ad0a11
____x_ <josuealexandre067@gmail.com>
2025-07-11 16:26:32 -0400

    Feat: Start 'react' project

---
+++ React/src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './global.css'
+
+import App from './app.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <StrictMode>
+        <App/>
+    </StrictMode>
+)
