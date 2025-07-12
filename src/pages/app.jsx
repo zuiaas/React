ef70ff7e0585463cf167e00ad9c462e8ac27716e
____x_ <josuealexandre067@gmail.com>
2025-07-12 11:37:36 -0400

    Ref: Exemplo de consumo (app.jsx)

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -4,32 +4,34 @@ import './app.css'
 export default function App() {
     const [users, setUsers] = useState([])
     const [loading, setLoading] = useState(true)
-  
+
     useEffect(() => {
-      fetch('https://jsonplaceholder.typicode.com/users')
+        fetch('https://jsonplaceholder.typicode.com/users')
         .then(res => res.json())
         .then(data => {
-          setUsers(data)
-          setLoading(false)
+            setUsers(data)
+            setLoading(false)
         })
+        
         .catch(err => {
-          console.error('Erro ao buscar usuários:', err)
-          setLoading(false)
+            console.error('Erro ao buscar usuários:', err)
+            setLoading(false)
         })
     }, [])
-  
+
     if (loading) return <p>Carregando...</p>
-  
+
     return (
-      <div>
-        <h1>Usuários</h1>
-        <ul>
-          {users.map(user => (
-            <li key={user.id}>
-              {user.name} — {user.email}
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div>
+            <h1>Usuários</h1>
+
+            <ul>
+                {users.map(user => (
+                    <li key={user.id}>
+                        {user.name} — {user.email}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
-  }
+}
